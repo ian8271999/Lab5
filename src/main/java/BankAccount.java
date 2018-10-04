6acce98dfa55d838f396cfeef297c1896512e488
Ian <iangoldberg827@gmail.com>
2018-10-03 20:27:48 -0500

    working on bank account and bank

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -12,27 +12,53 @@ public class BankAccount {
     /*
      * You may want to use this to distinguish between different kinds of accounts.
      */
+
     public enum BankAccountType {
+        /** Checkings */
         CHECKINGS,
+        /** Savings. */
         SAVINGS,
+        /** Student. */
         STUDENT,
+        /** Workplace. */
         WORKPLACE
     }
 
+
+     /** The account number. */
     private int accountNumber;
-    public BankAccountType accountType;
+    /** The type of account. */
+    private BankAccountType accountType;
+    /** The balance of the account. */
     private double accountBalance;
+    /** The owner's name. */
     private String ownerName;
-    public double interestRate;
+    /** The interest rate. */
+    private double interestRate;
+    /** The interest earned. */
     private double interestEarned;
 
-    public BankAccount(final String name, final BankAccountType accountCategory) {
-        /*
-         * Implement this function
-         */
+    /** Constructor for BankAccount class.
+     *
+     * @param name the name.
+     * @param accountCategory the category.
+     * @param balance the balance.
+     * @param earned the interest earned.
+     * @param number the account number.
+     * @param rate the interest rate.
+     */
+    public BankAccount(final String name, final BankAccountType accountCategory, final int number,
+                       final double balance, final double rate, final double earned) {
+         ownerName = name;
+         accountType = accountCategory;
+         accountNumber = number;
+         accountBalance = balance;
+         interestRate = rate;
+         interestEarned = earned;
+    }
+    /** gets account number. */
+    public int getAccountNumber() {
+        return accountNumber;
     }
 
-    /*
-     * Implement getters and setters as appropriate for private variables.
-     */
 }
